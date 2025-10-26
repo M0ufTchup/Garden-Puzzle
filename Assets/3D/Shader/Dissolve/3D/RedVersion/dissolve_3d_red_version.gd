cfcabb16e83bc3cd7c9e2b15fd2b5229131cd8be
Romain <mromain911@gmail.com>
2025-10-26 23:12:53 +0100

    Despawn VFX

---
+++ Assets/3D/Shader/Dissolve/3D/RedVersion/dissolve_3d_red_version.gd
@@ -0,0 +1,27 @@
+extends MeshInstance3D
+
+var testShader: bool = false
+@export var curve: Curve
+@export var loop : bool = false;
+@onready var particle: GPUParticles3D = $GPUParticles3D
+
+var time: float = 0.0
+var dir: int = 1
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		testShader = !testShader
+		
+
+func _process(delta):
+	if testShader:
+		time += delta * dir;
+		var material: ShaderMaterial = mesh.surface_get_material(0)
+		material.set_shader_parameter('DissolveAmount', curve.sample(time));
+		particle.emitting = true;
+		if time > 1: particle.emitting = false;
+		if loop:
+			if time > 1: dir = -1;
+			elif time < 0: dir = 1;
+			
+		time = clamp(time, 0, 1)
